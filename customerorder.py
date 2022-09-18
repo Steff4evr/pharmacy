926494f10aee16f708e417f2fb702478689aa8c6
Steffy Johnson <steffy.johnson@Steffys-MacBook-Pro.local>
2022-09-18 13:52:47 +1000

    Commit persistent of cart in session - 18 Sept

--- customerorder.py
+++ customerorder.py
@@ -5,7 +5,7 @@ from placingcustomerorder import placing_customer_order
 
 
 def customer_order():
-
+    cart = []
     while True:
         # Menu for getting customer order
         print("****************************************************************")
@@ -13,7 +13,8 @@ def customer_order():
         print("****************************************************************")            
         print("1. Search for medicine and add to customer order")
         print("2. Placing the customer order")
-        print("3. Exit to main menu")
+        print("3. Verify medicines in cart and confirm order")
+        print("4. Exit to main menu")        
         # capture user input
         try:
             user_option = int(input("Choose an option .. "))
@@ -24,8 +25,10 @@ def customer_order():
         if user_option == 1:
             search_drugs()
         elif user_option == 2:
-            placing_customer_order()
+            cart = placing_customer_order(cart)
         elif user_option == 3:
+            print(f"Cart Items : {cart}")
+        elif user_option == 4:
             exit()
 
 customer_order()
--- placingcustomerorder.py
+++ placingcustomerorder.py
@@ -4,8 +4,7 @@ import openpyxl
 import time
 
 
-def placing_customer_order():
-    search_results = []
+def placing_customer_order(medicine_cart):    
     found_med_flag = False
     qty_available_flag = False
     # Enter the med id
@@ -14,26 +13,34 @@ def placing_customer_order():
         print("****************************************************************")
         print("********************Placing Customer Order**********************")
         print("****************************************************************")
-        try:
-            med_id_user_input = int(
-                input("Enter the med id to add to cart.. To discontinue, enter 0 "))
-        except ValueError:
-            print("Invalid med id !")
-            time.sleep(2)
+        #User input of the med id
+        while True:
+            try:
+                med_id_user_input = int(input("Enter the med id to add to cart.. To discontinue, enter 0 : "))
+                break
+            except ValueError:
+                print("Invalid med id !")
+                time.sleep(2)
+                continue
+        #Return to Customer Order Menu    
         if med_id_user_input == 0:
-            return None
-        try:
-            med_qty_user_input = int(
-                input("Enter the quantity to add to cart.. To discontinue, enter 0 "))
-        except ValueError:
-            print("Invalid Quantity !")
-            time.sleep(2)            
+            return medicine_cart
+        #User input of med quantity
+        while True:
+            try:
+                med_qty_user_input = int(input("Enter the quantity to add to cart.. To discontinue, enter 0 : "))
+                break
+            except ValueError:
+                print("Invalid Quantity !")
+                time.sleep(2)
+                continue
+        #Return to Customer Order Menu
         if med_qty_user_input == 0:
-            return None
+            return medicine_cart
         #Open inventory
         medicine_inventory = openpyxl.load_workbook("MEDICINE_INVENTORY.xlsx")
         mi = medicine_inventory.active
-        # Search Inventory for the Medicine
+        # Search Inventory for the Medicine and quantity
         for i in range(2, mi.max_row+1):                            
             med_id = mi.cell(row=i, column=1)
             med_name = mi.cell(row=i, column=2)
@@ -43,14 +50,13 @@ def placing_customer_order():
                 available_med_qty = med_qty.value
                 if (med_qty_user_input <= int(med_qty.value)):
                     qty_available_flag = True             
-                    search_results.append({med_id.value,med_name.value,med_qty.value})
-                    print(f" Medicine Id : {search_results}", end="\n")
+                    medicine_cart.append({'med_id': med_id.value,'med_name': med_name.value,'med_qty': med_qty_user_input})
+                    print(f" Medicine Id : {medicine_cart}", end="\n")
         if found_med_flag is False:
-            print("Medicine Not Found")
-        if qty_available_flag is False:
-            print(f"There is not enough quantity available for this medicine in the inventory. Only {available_med_qty} available")
-        # Enter the qty needed
-        # Check if the med id the qty is available
-        # if yes then add to  invoice
-        # if qty not available then do not add to invoice
-        # loop this process until order is completed
+            print("Medicine Not Found !")
+        if found_med_flag is True and qty_available_flag is False:
+            print(f"There is not enough quantity available for this medicine in the inventory. Only {available_med_qty} available !")
+        
+        
+        
+        
