7736d1139407bc5438d0b481e6416706c9cd379a
Steffy Johnson <steffy.johnson@Steffys-MacBook-Pro.local>
2022-09-18 16:52:11 +1000

    Commit Med Inventory duplicate prevention check - 18 Sept

--- customerorder.py
+++ customerorder.py
@@ -14,7 +14,8 @@ def customer_order():
         print("1. Search for medicine and add to customer order")
         print("2. Placing the customer order")
         print("3. Verify medicines in cart and confirm order")
-        print("4. Exit to main menu")        
+        print("4. Clear cart")
+        print("5. Exit to main menu")
         # capture user input
         try:
             user_option = int(input("Choose an option .. "))
@@ -24,11 +25,18 @@ def customer_order():
             time.sleep(2)
         if user_option == 1:
             search_drugs()
+            time.sleep(2)
         elif user_option == 2:
             cart = placing_customer_order(cart)
+            time.sleep(2)
         elif user_option == 3:
             print(f"Cart Items : {cart}")
+            time.sleep(2)
         elif user_option == 4:
+            cart.clear()
+            print("Cart Emptied !")
+            time.sleep(2)
+        elif user_option == 5:
             exit()
 
 customer_order()
--- placingcustomerorder.py
+++ placingcustomerorder.py
@@ -4,7 +4,7 @@ import openpyxl
 import time
 
 
-def placing_customer_order(medicine_cart):    
+def placing_customer_order(medicine_cart):
     found_med_flag = False
     qty_available_flag = False
     # Enter the med id
@@ -44,14 +44,33 @@ def placing_customer_order(medicine_cart):
         for i in range(2, mi.max_row+1):                            
             med_id = mi.cell(row=i, column=1)
             med_name = mi.cell(row=i, column=2)
-            med_qty = mi.cell(row=i, column=4)               
+            med_qty = mi.cell(row=i, column=4)
             if (med_id_user_input == int(med_id.value)):
                 found_med_flag = True
                 available_med_qty = med_qty.value
                 if (med_qty_user_input <= int(med_qty.value)):
-                    qty_available_flag = True             
-                    medicine_cart.append({'med_id': med_id.value,'med_name': med_name.value,'med_qty': med_qty_user_input})
-                    print(f" Medicine Id : {medicine_cart}", end="\n")
+                    qty_available_flag = True                                   
+                    if  (next((i for i, x in enumerate(medicine_cart) if x["med_id"] == med_id.value), None) is not None):
+                        while True:
+                            try:
+                                med_duplicate_user_input = input("Med already present in cart. Do you want to replace the existing item in the cart? (y/n)")
+                                if (med_duplicate_user_input.lower() =='y'):
+                                    medicine_cart = list(filter(lambda i: i['med_id'] != med_id.value, medicine_cart))
+                                    medicine_cart.append({'med_id': med_id.value,'med_name': med_name.value,'med_qty': med_qty_user_input})
+                                    print("Medicine replaced !")
+                                    break
+                                elif (med_duplicate_user_input.lower() =='n'):
+                                    print("Operation cancelled . Item not added to cart...")
+                                    break
+                                else:
+                                    raise ValueError
+                            except ValueError:
+                                print("Invalid Entry (Only y/n) !")
+                                time.sleep(1)
+                                continue
+                    else:
+                        medicine_cart.append({'med_id': med_id.value,'med_name': med_name.value,'med_qty': med_qty_user_input})
+                    print(f" MEDICINE CART : {medicine_cart}", end="\n")
         if found_med_flag is False:
             print("Medicine Not Found !")
         if found_med_flag is True and qty_available_flag is False:
